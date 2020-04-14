41b349ce495ec9dab914c6499a3412d0696a5599
Anthony Rice <anthony.rice.15@cnu.edu>
2020-04-13 22:16:29 -0400

    Stylized the graphs a bit so that all information is showing

--- src/components/Graph/GraphSmall.js
+++ src/components/Graph/GraphSmall.js
@@ -10,35 +10,42 @@ import {
 
 const GraphSmall = (props) => {
     return (
-        <div className="GraphSmall">
-            {props.mode === "MFF" && <h2>Main Fitness Floor</h2>}
-            {props.mode === "CT" && <h2>Cardio Theater</h2>}
-            {props.mode === "PLM" && <h2>Plate Loaded Machines</h2>}
-            {props.mode === "HWR" && <h2>Heavy Weight Room</h2>}
-            {props.mode === "MPR1" && <h2>Multipurpose Room 1</h2>}
-            {props.mode === "MPR2" && <h2>Multipurpose Room 2</h2>}
-            <div >
-                <XYPlot
-                    yType="ordinal"
-                    width={300}
-                    height={300}
-                    xDomain={[0,30]}>
-                    <HorizontalGridLines />
-                    <VerticalGridLines/>
-                    <HorizontalBarSeries
-                        data={[
-                            {x: props.data[props.data.length -1], y: props.timestamp[props.timestamp.length -1]},
-                            {x: props.data[props.data.length -2], y: props.timestamp[props.timestamp.length -2]},
-                            {x: props.data[props.data.length -3], y: props.timestamp[props.timestamp.length -3]},
-                            {x: props.data[props.data.length -4], y: props.timestamp[props.timestamp.length -4]},
-                            {x: props.data[props.data.length -5], y: props.timestamp[props.timestamp.length -5]},
-                            {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -6]}
-                        ]}/>
-                    <XAxis />
-                    <YAxis />
-                </XYPlot>
+        <div>
+            {props.mode === "MFF" && <h2 style={{textAlign: 'center'}}>Main Fitness Floor</h2>}
+            {props.mode === "CT" && <h2 style={{textAlign: 'center'}}>Cardio Theater</h2>}
+            {props.mode === "PLM" && <h2 style={{textAlign: 'center'}}>Plate Loaded Machines</h2>}
+            {props.mode === "HWR" && <h2 style={{textAlign: 'center'}}>Heavy Weight Room</h2>}
+            {props.mode === "MPR1" && <h2 style={{textAlign: 'center'}}>Multipurpose Room 1</h2>}
+            {props.mode === "MPR2" && <h2 style={{textAlign: 'center'}}>Multipurpose Room 2</h2>}
+            <div className="GraphSmall">
+                    <XYPlot
+                        yType="ordinal"
+                        width={600}
+                        height={400}
+                        xDomain={[0,30]}
+                        margin={{left: "250"}}>
+                        <HorizontalGridLines />
+                        <VerticalGridLines/>
+
+                        <HorizontalBarSeries
+                            data={[
+                                {x: props.data[props.data.length -1], y: props.timestamp[props.timestamp.length -1]},
+                                {x: props.data[props.data.length -2], y: props.timestamp[props.timestamp.length -2]},
+                                {x: props.data[props.data.length -3], y: props.timestamp[props.timestamp.length -3]},
+                                {x: props.data[props.data.length -4], y: props.timestamp[props.timestamp.length -4]},
+                                {x: props.data[props.data.length -5], y: props.timestamp[props.timestamp.length -5]},
+                                {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -6]},
+                                {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -7]},
+                                {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -8]},
+                                {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -9]},
+                                {x: props.data[props.data.length -6], y: props.timestamp[props.timestamp.length -10]}
+                            ]}/>
+                        <XAxis />
+                        <YAxis />
+                    </XYPlot>
             </div>
         </div>
+
     );
 };
 
