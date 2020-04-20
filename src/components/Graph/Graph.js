d3f93dde92bd97d6f107752e2bb1df5bdb1e2d61
GriffinH1nes <GbullJohn12>
2020-04-19 22:01:38 -0400

    Before hints to bar graph

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalBarSeries, VerticalGridLines} from 'react-vis';
+import Hint from "react-vis/es/plot/hint";
 
-const Graph = (props) => {
-    return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>Last Updated:</h1>
-            <h1 style={{textAlign: 'center'}}>{props.timestamp[props.timestamp.length -1]}</h1>
-            <div className="Graph">
-                <XYPlot
-                    xType="ordinal"
-                    width={400}
-                    height={400}
-                    yDomain={[0,30]}
-                    color={"#0A2240"}>
-                    <HorizontalGridLines/>
-                    <VerticalGridLines/>
-                    <VerticalBarSeries
-                        data={[
-                            {x: "MFF", y: props.mff[props.mff.length -1]},
-                            {x: "CT", y: props.ct[props.ct.length -1]},
-                            {x: "PLM", y: props.plm[props.plm.length -1]},
-                            {x: "HWR", y: props.hwr[props.hwr.length -1]},
-                            {x: "MPR1", y: props.mpr1[props.mpr1.length -1]},
-                            {x: "MPR2", y: props.mpr2[props.mpr2.length -1]}
-                        ]}/>
-                    <XAxis />
-                    <YAxis />
-                </XYPlot>
+// TODO HINTS TO THE BAR GRAPH
+
+class Graph extends Component {
+    render() {
+        return (
+            <div>
+                <h1 style={{textAlign: 'center'}}>Last Updated:</h1>
+                <h1 style={{textAlign: 'center'}}>{this.props.timestamp[this.props.timestamp.length - 1]}</h1>
+                <div className="Graph">
+                    <XYPlot
+                        xType="ordinal"
+                        width={400}
+                        height={400}
+                        yDomain={[0, 30]}
+                        color={"#0A2240"}>
+                        <HorizontalGridLines/>
+                        <VerticalGridLines/>
+                        <VerticalBarSeries
+                            data={[
+                                {x: "MFF", y: this.props.mff[this.props.mff.length - 1]},
+                                {x: "CT", y: this.props.ct[this.props.ct.length - 1]},
+                                {x: "PLM", y: this.props.plm[this.props.plm.length - 1]},
+                                {x: "HWR", y: this.props.hwr[this.props.hwr.length - 1]},
+                                {x: "MPR1", y: this.props.mpr1[this.props.mpr1.length - 1]},
+                                {x: "MPR2", y: this.props.mpr2[this.props.mpr2.length - 1]}
+                            ]}
+                            onNearestX={this._onNearestX}
+                        />
+                        <XAxis/>
+                        <YAxis/>
+
+                    </XYPlot>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Graph;
